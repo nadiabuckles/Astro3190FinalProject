b53e72a3189cdf60f918eb32f277390c00fb2133
nadiabuckles <nadia.buckles@ou.edu>
2016-05-10 21:27:00 -0500

    Final version
    moved printing of max data into dialog box, added more limit input comments

--- Full_GUI_2_1.py
+++ Full_GUI_2_1.py
@@ -151,19 +151,17 @@ class SpectraApp(tk.Tk):
         #self.EntryName doesn't work
         #EntryName can't be defined as global
         #passing self as argument to EntryName.get() doesn't help
+        #passing EntryName as a function argument doesn't work
         mbox.showerror("Error", "This feature is not available")
 
     def FitSpectra(self):
 
         if data != None:
             epos, mi = pyasl.quadExtreme(x, y, mode ="max", dp = (5, 5))
-           
-            print (mi)
-            print (data[mi, 1])
-            print (epos)
 
-            mbox.showinfo("Quadratic Fitting Results\n")
-             
+            fitinfo = [('Index of Maximum', mi), ('Value at Maximum', data[mi, 1]), ('Maximum found by parabolic fit', epos)]
+
+            mbox.showinfo("Quadratic Fitting Results\n", fitinfo) 
         else:
             mbox.showerror("Error", "No data has been entered")
 
